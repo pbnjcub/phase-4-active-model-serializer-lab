21a6879d0a3bac325febfde8c7309484bd03dd40
Paul Jung <pbnjcub@gmail.com>
2023-03-25 15:13:48 -0400

    commit tree a83ad34be12278a9edb6b5bc264d4674afc1b328

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,10 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :content
+  attributes :title, :content, :short_content
 
   belongs_to :author
   has_many :tags
 
   def short_content
-    posts.content.length > 40 ? "#{posts.content[0..39]}..." : posts.content
+    object.content.truncate(43, omission: '...')
+    # "#{self.object.content[0-39]...}"
   end
 end
